8eef500ab2c8f8bb48b56d083f2e7d490e554401
George Jabbour <georgejabbour3@gmail.com>
2018-06-21 19:21:05 -0500

    finished balancing brackets task

--- coursera/DataStructures/Programming-Assignment-1/check_brackets_in_code/check_brackets.py
+++ coursera/DataStructures/Programming-Assignment-1/check_brackets_in_code/check_brackets.py
@@ -6,7 +6,6 @@ class Bracket:
     def __init__(self, bracket_type, position):
         self.bracket_type = bracket_type
         self.position = position
-
     def Match(self, c):
         if self.bracket_type == '[' and c == ']':
             return True
@@ -16,17 +15,36 @@ class Bracket:
             return True
         return False
 
-if __name__ == "__main__":
-    text = sys.stdin.read()
+def PopFront(stack):
+    if len(stack)==0:
+        return
+    return stack.pop(0)
 
+def BracketChecker(text):
     opening_brackets_stack = []
+    count=0
+    # print(text,", ", len(text))
     for i, next in enumerate(text):
+        # print("i'm enumerating")
+        count+=1
+        # print("i: ",i)
         if next == '(' or next == '[' or next == '{':
-            # Process opening bracket, write your code here
-            pass
+            # print("next is an opening bracket: ",next)
+            b=Bracket(next,i+1)
+            opening_brackets_stack.insert(0,b)
+        elif next == ')' or next == ']' or next == '}':
+            # print("next is a closing bracket: ",next)
+            if len(opening_brackets_stack)==0:
+                # print("len(opening_brackets_stack) was 0")
+                return i+1
+            top=PopFront(opening_brackets_stack)
+            # print("top: ",top.bracket_type,", next: ",next)
+            if (top.bracket_type=='(' and next!=')') or (top.bracket_type=='[' and next!=']') or (top.bracket_type=='{' and next!='}'):
+                return i+1
+    if len(opening_brackets_stack)==0:
+        return "Success"
+    return opening_brackets_stack[0].position
 
-        if next == ')' or next == ']' or next == '}':
-            # Process closing bracket, write your code here
-            pass
-
-    # Printing answer, write your code here
+if __name__ == "__main__":
+    text = input()
+    print(BracketChecker(text))
